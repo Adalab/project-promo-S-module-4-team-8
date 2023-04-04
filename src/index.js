c4fdb524eccf012a564e42cc4a40884e488c43a6
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-04-04 14:10:08 +0200

    add express

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 // importaciones
-const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql2/promise')
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2/promise");
 
 // configurar el servidor
 const server = express();
 server.use(cors());
-server.use(express.json({ limit: '10mb' }));
+server.use(express.json({ limit: "10mb" }));
 
 //arrancar el servidor
 const serverPort = 4000;
@@ -14,42 +14,42 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-//servidor de estáticos
-// server.use(express.static('web/public'));
-
 //crear conexión con base de datos
 
-let connection;  // Aquí almacenaremos la conexión a la base de datos
+let connection; // Aquí almacenaremos la conexión a la base de datos
 
 mysql
   .createConnection({
-    host: 'sql.freedb.tech',
-    database: 'freedb_project-mod-4-tema-8',
-    user: 'freedb_root-beafig',
-    password: 'nU4ywdU!5VJJ#js',
+    host: "sql.freedb.tech",
+    database: "freedb_project-mod-4-tema-8",
+    user: "freedb_root-beafig",
+    password: "nU4ywdU!5VJJ#js",
   })
-  .then(conn => {
+  .then((conn) => {
     connection = conn;
     connection
       .connect()
       .then(() => {
-        console.log(`Conexión establecida con la base de datos (identificador=${connection.threadId})`);
+        console.log(
+          `Conexión establecida con la base de datos (identificador=${connection.threadId})`
+        );
       })
       .catch((err) => {
-        console.error('Error de conexion: ' + err.stack);
+        console.error("Error de conexion: " + err.stack);
       });
   })
   .catch((err) => {
-    console.error('Error de configuración: ' + err.stack);
+    console.error("Error de configuración: " + err.stack);
   });
 
 // petición GET al servidor
-server.get('/api/projects/all', (req, res) => {
-  let sql = 'SELECT * FROM projects, autors WHERE projects.fk_autors = autors.idAutor';
+server.get("/api/projects/all", (req, res) => {
+  let sql =
+    "SELECT * FROM projects, autors WHERE projects.fk_autors = autors.idAutor";
   connection
     .query(sql)
     .then(([results, fields]) => {
-      console.log('Información recuperada:');
+      console.log("Información recuperada:");
       results.forEach((result) => {
         console.log(result);
       });
@@ -61,3 +61,53 @@ server.get('/api/projects/all', (req, res) => {
     });
 });
 
+server.post("/api/projects/add", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  let sqlAutor = "insert into autors (autor, job, image ) values (?, ?, ?)";
+  let valuesAutor = [data.autor, data.job, data.image];
+
+  connection
+    .query(sqlAutor, valuesAutor)
+    .then(([results]) => {
+      console.log(results);
+      let sqlProject =
+        "insert into projects (name, slogan, repo, demo, technologies, description, photo, fk_autors ) values (?, ?, ?, ?, ?, ?, ?, ?)";
+      let valuesProject = [
+        data.name,
+        data.slogan,
+        data.repo,
+        data.demo,
+        data.technologies,
+        data.desc,
+        data.photo,
+        results.insertId,
+      ];
+      connection
+        .query(sqlProject, valuesProject)
+        .then(([results]) => {
+          let response = {
+            success: true,
+            cardURL: `http://localhost:4000/api/projects/${results.insertId}`,
+          };
+          res.json(response);
+        })
+        .catch((err) => {
+          throw err;
+        });
+    })
+    .catch((err) => {
+      throw err;
+    });
+});
+// server.get("/api/project/:id", (req, res) => {
+//   const relativePath = "./web/src/components/Main/CardDetail.js";
+//   const absolutePath
+//   connection
+//     .query("SELECT * FROM projects, autors WHERE id=?", [req.params.id])
+//     .then(([results]) => {
+//       res.render();
+//     });
+// });
+//servidor de estáticos
+server.use(express.static("./src/public-react"));
--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,26 +1,24 @@
 const dataApi = (data) => {
-  return fetch('https://dev.adalab.es/api/projectCard',
-    {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: { 'Content-type': 'application/json' }
-    })
-    .then(response => response.json())
-    .then(data => {
+  return fetch("http://localhost:4000/api/projects/add", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then((data) => {
       return data;
-    })
-}
+    });
+};
 
 // obtener el listado
 
 const listProjectApi = () => {
-  return fetch('http://localhost:4000/api/projects/all')
-    .then(response => response.json())
-    .then(data => {
+  return fetch("http://localhost:4000/api/projects/all")
+    .then((response) => response.json())
+    .then((data) => {
       console.log(data);
-      return data
-    })
-}
-
+      return data;
+    });
+};
 
 export default { dataApi, listProjectApi };
