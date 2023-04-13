eda46520dd45a8be699169d2f9d20dd8ab21f8e5
beafig <beafigueroa89@gmail.com>
2023-04-13 21:51:13 +0200

    Publish-react

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,5 +1,5 @@
 const dataApi = (data) => {
-  return fetch("https://gestor-proyectos.onrender.com/api/projects/add", {
+  return fetch("gestor-proyectos.onrender.com/api/projects/add", {
     method: "POST",
     body: JSON.stringify(data),
     headers: { "Content-type": "application/json" },
@@ -13,7 +13,7 @@ const dataApi = (data) => {
 // obtener el listado
 
 const listProjectApi = () => {
-  return fetch("https://gestor-proyectos.onrender.com/api/projects/all")
+  return fetch("gestor-proyectos.onrender.com/api/projects/all")
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
